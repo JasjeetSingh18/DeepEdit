2fc2693ee762b055926ffcb316a5212a39d73ff7
JasjeetSingh18 <jasjeet150@gmail.com>
2025-09-01 19:38:19 -0400

    update: Fixed Image AI enhancer

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import enhanceImage
+from PIL import Image
 
 # Configure application
 app = Flask(__name__)
